8d8ded727babb0c38b80576bb2d2ccdad841672e
mildmocha <128768439+mildmocha@users.noreply.github.com>
2023-06-14 17:38:29 +0900

    Add files via upload

--- src/component/Wrap.jsx
+++ src/component/Wrap.jsx
@@ -3,10 +3,10 @@ import classes from './Wrap.module.css';
 const Wrap = ({children}) => {
   const style1 = {
     minWidth : '300px',
-    
+    width: '100%',
     minHeight: '500px',
     position: 'relative'
-   , //background :  "#FFC125" 
+   , background :  "#FFC125" 
   }
 
 return (
